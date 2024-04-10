0a874dd82a522973e73c0e8c5d08c29857cafab0
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2024-04-10 16:06:49 +0200

    Refactor: Code refactored and simplified.

--- src/router/router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getReviews } from "../controllers/controllers.js";
-
-const router = Router();
-
-router.get("/getReviews", getReviews);
-
-export { router };
